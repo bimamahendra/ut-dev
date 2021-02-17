0f61606991a38849eebb8b086eb38040653ddf0c
devisap <ilham.sagitaputra@gmail.com>
2021-02-17 10:44:58 +0700

    init RestServer

---
+++ application/controllers/api/User.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+// require APPPATH . '/libraries/REST_Controller.php';
+use chriskacerguis\RestServer\RestController;
+
+class User extends RestController {
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper("url");
+        $this->load->library(array('upload', 'image_lib'));
+    }
+
+    public function index_get(){
+        $this->response(['status' => TRUE, 'message' => "OKE"], 200);
+    }
+}
