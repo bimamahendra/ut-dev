fdf1410354a2f6619665a749493dcf1eab0ba07f
Gellen <hellogellen@gmail.com>
2021-02-19 19:45:21 +0700

    add favicon

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -8,12 +8,13 @@
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css'); ?>" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/css/sb-admin-2.min.css'); ?>" rel="stylesheet">
+
+    <link rel="icon" href="<?= base_url('assets/img/favicon/favicon-16x16.png'); ?>" sizes="16x16">
+    <link rel="icon" href="<?= base_url('assets/img/favicon/favicon-32x32.png'); ?>" sizes="32x32">
 
     <title>United Tracktor - Login</title>
 </head>
