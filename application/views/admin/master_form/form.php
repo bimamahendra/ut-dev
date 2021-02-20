67d171d0bb72a1235140c302df1503aa508e7d3d
Gellen <hellogellen@gmail.com>
2021-02-20 21:12:58 +0700

    split modal

--- application/views/admin/master_form/form.php
+++ application/views/admin/master_form/form.php
@@ -12,7 +12,7 @@
         <div class="card-header py-3">
             <div class="d-sm-flex align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-warning">Form Table</h6>
-                <button class="d-none d-sm-inline-block btn btn-sm btn-warning shadow-sm" data-toggle="modal" data-target="#formTableModal">
+                <button class="d-none d-sm-inline-block btn btn-sm btn-warning shadow-sm" data-toggle="modal" data-target="#mdlAdd">
                     <i class="fas fa-plus fa-sm text-white-50"></i>
                     Add
                 </button>
@@ -44,7 +44,7 @@
                                 <a href="<?= site_url('welcome/list_approval'); ?>" class="btn btn-info btn-sm">
                                     <i class="fa fa-cog"></i>
                                 </a>
-                                <button type="button" data-toggle="modal" data-target="#deleteFormItem" class="btn btn-danger btn-sm">
+                                <button type="button" data-toggle="modal" data-target="#mdlDelete" class="btn btn-danger btn-sm">
                                     <i class="fa fa-trash"></i>
                                 </button>
                             </td>
@@ -62,3 +62,76 @@
 
 </div>
 <!-- End of Main Content -->
+
+<!-- Modal Add -->
+<div class="modal fade" id="mdlAdd" tabindex="-1" aria-labelledby="mdlAdd" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="mdlAdd">Add Form</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="col">
+                    <select class="custom-select" required>
+                        <option value="" selected>Nama Tabel</option>
+                        <option>Tabel 1</option>
+                        <option>Tabel 2</option>
+                        <option>Tabel 3</option>
+                    </select>
+                </div>
+            </div>
+            <div class="modal-body">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="No Doc">
+                </div>
+            </div>
+            <div class="modal-body">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Nama Form">
+                </div>
+            </div>
+            <div class="modal-body">
+                <div class="col">
+                    <select class="custom-select" name="DIV_USERS" required>
+                        <option value="" selected>Divisi</option>
+                        <option value="Project Management">Project Management</option>
+                        <option value="General Service & Maintenances Management">General Service & Maintenance Management</option>
+                        <option value="Budget, Asset & Building Management">Budget, Asset & Building Management</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-warning">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Delete -->
+<div class="modal fade" id="mdlDelete" tabindex="-1" aria-labelledby="mdlDelete" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="mdlDelete">Delete Item?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    Anda akan menghapus item "Table 1"
+                </p>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                <button type="button" class="btn btn-danger">Hapus</button>
+            </div>
+        </div>
+    </div>
+</div>
--- application/views/admin/master_form/list_approval.php
+++ application/views/admin/master_form/list_approval.php
@@ -12,7 +12,7 @@
         <div class="card-header py-3">
             <div class="d-sm-flex align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-warning">List Approval</h6>
-                <button class="d-none d-sm-inline-block btn btn-sm btn-warning shadow-sm" data-toggle="modal" data-target="#settingListApproval">
+                <button class="d-none d-sm-inline-block btn btn-sm btn-warning shadow-sm" data-toggle="modal" data-target="#mdlAdd">
                     <i class="fas fa-plus fa-sm text-white-50"></i>
                     Add
                 </button>
@@ -33,10 +33,10 @@
                             <td>1</td>
                             <td>Section Head</td>
                             <td>
-                                <button type="button" data-toggle="modal" data-target="#editListApproval" class="btn btn-primary btn-sm">
+                                <button type="button" data-toggle="modal" data-target="#mdlEdit" class="btn btn-primary btn-sm">
                                     <i class="fa fa-edit"></i>
                                 </button>
-                                <button type="button" data-toggle="modal" data-target="#deleteListApproval" class="btn btn-danger btn-sm">
+                                <button type="button" data-toggle="modal" data-target="#mdlDelete" class="btn btn-danger btn-sm">
                                     <i class="fa fa-trash"></i>
                                 </button>
                             </td>
@@ -54,3 +54,85 @@
 
 </div>
 <!-- End of Main Content -->
+
+<!-- Modal Add -->
+<div class="modal fade" id="mdlAdd" tabindex="-1" aria-labelledby="mdlAdd" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="mdlAdd">Setting List Approval</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="col">
+                    <select class="custom-select" required>
+                        <option value="" selected>Divisi</option>
+                        <option>Project Management</option>
+                        <option>General Service</option>
+                        <option>Maintenance Management</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                <button type="button" class="btn btn-warning">Save Changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Edit -->
+<div class="modal fade" id="mdlEdit" tabindex="-1" aria-labelledby="mdlEdit" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="mdlEdit">Edit List Approval</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="col">
+                    <select class="custom-select" required>
+                        <option value="" selected>Divisi</option>
+                        <option>Project Management</option>
+                        <option>General Service</option>
+                        <option>Maintenance Management</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                <button type="button" class="btn btn-warning">Save Changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Delete -->
+<div class="modal fade" id="mdlDelete" tabindex="-1" aria-labelledby="mdlDelete" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="mdlDelete">Delete List Approval?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    Anda akan menghapus list Approval "Section Head"
+                </p>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                <button type="button" class="btn btn-danger">Hapus</button>
+            </div>
+        </div>
+    </div>
+</div>
