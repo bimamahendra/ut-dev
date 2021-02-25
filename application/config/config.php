27a5c5f85f15fc5b790c25ebb1bb18b0ddaf93eb
devisap <ilham.sagitaputra@gmail.com>
2021-02-25 12:38:22 +0700

    generate pdf api

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'https://ut-dev.bgskr-project.my.id/';
+// $config['base_url'] = 'https://ut-dev.bgskr-project.my.id/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
