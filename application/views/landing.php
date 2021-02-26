5dc7264dc586f95aa74dc2cfe6da356d39462a12
Gellen <hellogellen@gmail.com>
2021-02-26 14:56:20 +0700

    update landing & header

--- application/views/landing.php
+++ application/views/landing.php
@@ -1,16 +1,10 @@
-<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
-    <div class="d-flex justify-content-between w-100">
-        <a class="navbar-brand my-2 my-sm-0" href="#">United Tractors</a>
-        <a class="btn btn-warning my-2 my-sm-0" href="<?= site_url('register'); ?>">Register</a>
-    </div>
-</nav>
-
 <div class="bg-image-ut">
     <div class="container">
-        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center min-vh-100 pt-5">
+        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center min-vh-100">
             <div class="d-flex flex-column justify-content-center order-2 order-md-1 mt-6">
                 <img src="<?= base_url('assets/img/landing/qr_app.png') ?>" width="150" alt="QR Code App">
                 <h4 class="text-white my-3">Install Android App</h4>
+                <a class="btn btn-warning my-2 my-sm-0" href="<?= site_url('register'); ?>">Register</a>
                 <h5 class="text-white">&copy; United Tractors</h5>
             </div>
             <img class="mb-4 mt-5 mt-md-0 order-1 order-md-2" src="<?= base_url('assets/img/landing/app_splash.png') ?>" width="250" alt="Splash App">
