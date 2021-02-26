42a0b17e08bb5c72645532facc33c0e3d19dbff0
Gellen <hellogellen@gmail.com>
2021-02-26 15:36:56 +0700

    update transaksi, sidebar, topbar

--- application/views/admin/master_transaksi/transaksi.php
+++ application/views/admin/master_transaksi/transaksi.php
@@ -10,12 +10,14 @@
         <div class="card-header py-3">
             <div class="d-sm-flex align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-warning mb-2">Transaction List</h6>
+                <!--
                 <select class="custom-select" style="width: 30%;" required>
                     <option value="" selected>Divisi</option>
                     <option>Project Management</option>
                     <option>General Service & Maintenance Management</option>
                     <option>Budget, Asset & Building Management</option>
                 </select>
+                -->
             </div>
         </div>
         <div class="card-body">
@@ -27,7 +29,7 @@
                             <th>Applicant</th>
                             <th>Form</th>
                             <th>Timestamp</th>
-                            <th>Link</th>
+                            <th>Keterangan</th>
                             <th>Flag</th>
                             <th>Status</th>
                             <th>Action</th>
@@ -35,46 +37,46 @@
                     </thead>
                     <tbody>
                         <?php
-                            foreach ($trans as $item) {
-                                $approvalBtn = '';
-                                if($item->STAT_TRANS == '0'){
-                                    $status = 'Unverified';
-                                    $approvalBtn = '
-                                        <button type="button" data-toggle="modal" data-target="#mdlApprove" data-id="'.$item->ID_TRANS.'" class="btn btn-success btn-sm mx-1 rounded mdlApprove" data-tooltip="tooltip" data-placement="top" title="Approve">
+                        foreach ($trans as $item) {
+                            $approvalBtn = '';
+                            if ($item->STAT_TRANS == '0') {
+                                $status = 'Unverified';
+                                $approvalBtn = '
+                                        <button type="button" data-toggle="modal" data-target="#mdlApprove" data-id="' . $item->ID_TRANS . '" class="btn btn-success btn-sm mx-1 rounded mdlApprove" data-tooltip="tooltip" data-placement="top" title="Approve">
                                             <i class="fa fa-check"></i>
                                         </button>
-                                        <button type="button" data-toggle="modal" data-target="#mdlReject" data-id="'.$item->ID_TRANS.'" class="btn btn-danger btn-sm rounded mdlReject" data-tooltip="tooltip" data-placement="top" title="Reject">
+                                        <button type="button" data-toggle="modal" data-target="#mdlReject" data-id="' . $item->ID_TRANS . '" class="btn btn-danger btn-sm rounded mdlReject" data-tooltip="tooltip" data-placement="top" title="Reject">
                                             <i class="fa fa-times"></i>
                                         </button>
                                     ';
-                                }else if($item->STAT_TRANS == '1'){
-                                    $status = 'Verified';
-                                }else if($item->STAT_TRANS == '2'){
-                                    $status = 'Finished';
-                                }else if($item->STAT_TRANS == '3'){
-                                    $status = 'Rejected';
-                                }
+                            } else if ($item->STAT_TRANS == '1') {
+                                $status = 'Verified';
+                            } else if ($item->STAT_TRANS == '2') {
+                                $status = 'Finished';
+                            } else if ($item->STAT_TRANS == '3') {
+                                $status = 'Rejected';
+                            }
 
-                                echo '
+                            echo '
                                     <tr>
                                         <td>1</td>
-                                        <td>'.$item->NAMA_USERS.'</td>
-                                        <td>'.$item->NAMA_FORM.'</td>
-                                        <td>'.$item->TS_TRANS.'</td>
-                                        <td><a href="'.$item->PATH_TRANS.'">'.$item->PATH_TRANS.'</a></td>
-                                        <td>'.$item->FLAG_TRANS.'</td>
-                                        <td>'.$status.'</td>
+                                        <td>' . $item->NAMA_USERS . '</td>
+                                        <td>' . $item->NAMA_FORM . '</td>
+                                        <td>' . $item->TS_TRANS . '</td>
+                                        <td>' . $item->KETERANGAN_TRANS . '</td>
+                                        <td>' . $item->FLAG_TRANS . '</td>
+                                        <td>' . $status . '</td>
                                         <td>
                                             <div class="btn-group" role="group">
-                                                <button type="button" data-toggle="modal" data-target="#mdlView" data-src="'.$item->PATH_TRANS.'" class="btn btn-primary btn-sm rounded mdlView" data-tooltip="tooltip" data-placement="top" title="Detail">
+                                                <button type="button" data-toggle="modal" data-target="#mdlView" data-src="' . $item->PATH_TRANS . '" class="btn btn-primary btn-sm rounded mdlView" data-tooltip="tooltip" data-placement="top" title="Detail">
                                                     <i class="fa fa-eye"></i>
                                                 </button>
-                                                '.$approvalBtn.'
+                                                ' . $approvalBtn . '
                                             </div>
                                         </td>
                                     </tr>
                                 ';
-                            }
+                        }
                         ?>
                     </tbody>
                 </table>
@@ -106,7 +108,7 @@
 
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                <form action="<?= site_url('transaction/approve')?>" method="post">
+                <form action="<?= site_url('transaction/approve') ?>" method="post">
                     <input type="hidden" id="mdlApprove_id" name="ID_TRANS" />
                     <button type="submit" class="btn btn-warning">Approve</button>
                 </form>
@@ -117,28 +119,28 @@
 <!-- Modal Reject -->
 <div class="modal fade" id="mdlReject" tabindex="-1" aria-labelledby="mdlReject" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="mdlReject">Reject Item?</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                <form action="<?= site_url('transaction/reject')?>" method="post">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="mdlReject">Reject Item?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= site_url('transaction/reject') ?>" method="post">
                     <div class="form-group">
                         <label for="inputKeterangan">Keterangan</label>
                         <textarea name="KETERANGAN_TRANS" class="form-control" required></textarea>
                     </div>
-                </div>
-
-                <div class="modal-footer">
-                    <input type="hidden" id="mdlReject_id" name="ID_TRANS" />
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                    <button type="submit" class="btn btn-danger">Reject</button>
-                </form>
-                </div>
             </div>
+
+            <div class="modal-footer">
+                <input type="hidden" id="mdlReject_id" name="ID_TRANS" />
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                <button type="submit" class="btn btn-danger">Reject</button>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 <!-- Modal View PDF -->
@@ -171,15 +173,15 @@
             width: 'resolve'
         });
     });
-    $('#tableTransaction tbody').on('click', '.mdlApprove', function(){
+    $('#tableTransaction tbody').on('click', '.mdlApprove', function() {
         const id = $(this).data("id")
         $('#mdlApprove_id').val(id)
     })
-    $('#tableTransaction tbody').on('click', '.mdlReject', function(){
+    $('#tableTransaction tbody').on('click', '.mdlReject', function() {
         const id = $(this).data("id")
         $('#mdlReject_id').val(id)
     })
-    $('#tableTransaction tbody').on('click', '.mdlView', function(){
+    $('#tableTransaction tbody').on('click', '.mdlView', function() {
         const src = $(this).data("src")
         $('#mdlView_src').attr('src', src);
     })
