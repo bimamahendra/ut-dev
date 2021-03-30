5a682846311fa22d94277d41049de10c95d81043
Geeree <79096553+dangiriel@users.noreply.github.com>
2021-03-30 21:21:21 +0700

    api permintaan layout acara

---
+++ application/controllers/api/FormLayout.php
@@ -0,0 +1,106 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+// require APPPATH . '/libraries/REST_Controller.php';
+use chriskacerguis\RestServer\RestController;
+
+class FormLayout extends RestController {
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper("url");
+        $this->load->library(array('upload', 'image_lib'));
+    }
+
+    public function index_post(){
+        $param = $this->post();
+        if(!empty($param['idUser']) && !empty($param['idMapping'])){
+            $user       = $this->db->get_where('USERS', ['ID_USERS' => $param['idUser']])->result();
+            $mapping    = $this->db->get_where('MAPPING', ['ID_MAPPING' => $param['idMapping']])->result();
+            if($user != null && $mapping != null){
+                $idTrans        = 'TRANS_'.substr(md5(time()."trans"), 0, 14);
+                $idLayout       = 'LAYOUT_'.substr(md5(time()."layout"), 0, 13);
+                
+                $storeTransaksi['ID_TRANS']         = $idTrans;
+                $storeTransaksi['ID_USERS']         = $param['idUser'];
+                $storeTransaksi['ID_MAPPING']       = $param['idMapping'];
+                $this->db->insert('TRANSACTION', $storeTransaksi);
+
+                $storeLayout['ID_LAYOUT']        = $idLayout;
+                $storeLayout['ID_TRANS']         = $idTrans;
+                $storeLayout['NAMA_LAYOUT']      = $param['namaAcara'];
+                $storeLayout['TGL_LAYOUT']       = $param['tglJam'];
+                $storeLayout['PESERTA_LAYOUT']   = $param['peserta'];
+                $storeLayout['LOKASI_LAYOUT']    = $param['lokasi'];
+                $storeLayout['BIAYA_LAYOUT']     = $param['biaya'];
+                $storeLayout['KET_LAYOUT']       = $param['keterangan'];
+                $this->db->insert('FORM_LAYOUT', $storeLayout);
+                
+                $flow = $this->db->get_where('FLOW', ['ID_MAPPING' => $mapping[0]->ID_MAPPING])->result_array();
+                for($i = 1; $i <= 15; $i++){
+                    if(!empty($flow[0]['APP_'.$i]) && $flow[0]['APP_'.$i] != null){
+                        $this->db->insert('DETAIL_APPROVAL', ['ID_TRANS' => $idTrans, 'ROLE_APP' => $flow[0]['APP_'.$i]]);
+                    }else if($flow[0]['APP_1'] == null){
+                        $this->db->where('ID_TRANS', $idTrans)->update('TRANSACTION', ['STAT_TRANS' => '2']);
+                    }
+                }
+                
+                $this->pusherjs->push();
+                $this->response(['status' => true, 'message' => 'Data berhasil ditambahkan', 'idTrans' => $idTrans], 200);
+            }else{
+                $this->response(['status' => false, 'message' => 'Data user atau mapping tidak ditemukan'], 200);
+            }
+        }else{
+            $this->response(['status' => false, 'message' => 'Parameter tidak cocok'], 200);
+        }
+    }
+    public function upload_post(){
+        $param = $this->post();
+        if(!empty($param['idTrans']) && !empty($param['idUser'])){
+            $layout = $this->db->get_where('FORM_LAYOUT', ['ID_TRANS' => $param['idTrans']])->result();
+            $user   = $this->db->get_where('USERS', ['ID_USERS' => $param['idUser']])->result();
+            if($user != null && $layout != null){
+                $file = $this->upload_image($user[0]->USER_USERS);
+                $this->db->where('ID_TRANS', $param['idTrans'])->update('FORM_LAYOUT', ['IMG_LAYOUT' => $file]);
+
+                $this->ContentPdf->generate(['idTrans' => $param['idTrans'], 'orientation' => 'portrait']);
+                $this->response(['status' => true, 'message' => 'Data berhasil ditambahkan'], 200);
+            }else{
+                $this->response(['status' => false, 'message' => 'Data user atau transaksi mobil pribadi tidak ditemukan'], 200);
+            }
+        }else{
+            $this->response(['status' => false, 'message' => 'Parameter tidak cocok'], 200);
+        }
+    }
+    
+    function upload_image($username){
+        $newPath = './uploads/assets/acara/'.$username.'/';
+        if(!is_dir($newPath)){
+            mkdir($newPath, 0777, TRUE);
+        }
+        $config['upload_path'] = $newPath;
+        $config['allowed_types'] = 'gif|jpg|png|jpeg|bmp';
+        $config['encrypt_name'] = TRUE;
+ 
+        $this->upload->initialize($config);
+        if(!empty($_FILES['file']['name'])){
+ 
+            if ($this->upload->do_upload('file')){
+                $gbr = $this->upload->data();
+                //Compress Image
+                $config['image_library']='gd2';
+                $config['source_image']=$newPath.$gbr['file_name'];
+                $config['create_thumb']= FALSE;
+                $config['maintain_ratio']= true;
+                $config['width']= 600;
+                $config['new_image']= $newPath.$gbr['file_name'];
+                $this->load->library('image_lib', $config);
+                $this->image_lib->resize();
+ 
+                $gambar=$gbr['file_name'];
+
+                return base_url('/uploads/assets/acara/'.$username.'/'.$gambar);
+            }
+                      
+        }else{
+            return base_url('images/ttd/default.png');
+        }
+    }
+}
