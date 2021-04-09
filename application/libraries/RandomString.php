1bc697314dfa5575b9f56f6c584804360614d374
devisap <ilham.sagitaputra@gmail.com>
2021-04-10 01:58:52 +0700

    fixing libraries randomstring 2

--- application/libraries/RandomString.php
+++ application/libraries/RandomString.php
@@ -1,4 +1,5 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
     class Randomstring {
         function getRandString($length) {
             $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
