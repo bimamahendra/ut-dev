287d1af0c1764159f84cc757a444c4740574d176
devisap <ilham.sagitaputra@gmail.com>
2021-08-27 21:09:30 +0700

    approval cek form non asset 2

--- application/controllers/api/FormNonAsset.php
+++ application/controllers/api/FormNonAsset.php
@@ -41,7 +41,7 @@ class FormNonAsset extends RestController {
                 $flow = $this->db->get_where('FLOW', ['ID_MAPPING' => $mapping[0]->ID_MAPPING])->result_array();
                 for($i = 1; $i <= 15; $i++){
                     if(!empty($flow[0]['APP_'.$i]) && $flow[0]['APP_'.$i] != null){
-                        if($param['isDivApprov'] == "2" && $flow[0]['APP_'.$i != "Division Head"]){
+                        if($param['isDivApprove'] == "2" && $flow[0]['APP_'.$i != "Division Head"]){
                             $this->db->insert('DETAIL_APPROVAL', ['ID_TRANS' => $idTrans, 'ROLE_APP' => $flow[0]['APP_'.$i]]);
                         }
                     }else if($flow[0]['APP_1'] == null){
